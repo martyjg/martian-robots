e416d337458c4abe1e00394abe9c6c3205730083
martyjg <martinjosephgormley@gmail.com>
2016-04-17 19:13:16 +0100

    Pass the initial direction and move of a robot and log the information

--- app.js
+++ app.js
@@ -37,13 +37,89 @@ function setRobot(command, serialNumber) {
 
   var initialRobotX = parseInt(command.split(" ", 1)[0]);
   var initialRobotY = parseInt(command.split(" ", 2)[1]);
+  var initialDirection = command.split(" ", 3)[2];
+  var moveSequence = command.split(" ", 4)[3];
 
   grid[initialRobotX][initialRobotY] = "ROBOT " + serialNumber;
 
+  nextMove(initialRobotX, initialRobotY, serialNumber, initialDirection, moveSequence);
+
   console.log(grid);
+  console.log("initialDirection", initialDirection); 
 }
 
+function nextMove(xPos, yPos, serialNumber, direction, moveSequence) {
+  console.log("ROBOT " + serialNumber + " is at position " + xPos + yPos + " and is facing towards " + direction);
 
+  for (i = 0; i < moveSequence.length; i++) {
+    computeMove(xPos, yPos, serialNumber, direction, moveSequence[i]);
+  }
+
+}
+
+function computeMove(xPos, yPos, serialNumber, direction, move) {
+
+  if (move === "F") {
+
+    switch(direction) {
+      case "N":
+      yPos++;
+      checkForScent(grid[xPos][yPos]);
+      break;
+      case "S":
+      yPos--
+      checkForScent(grid[xPos][yPos]);
+      break;
+      case "E":
+      xPos++
+      checkForScent(grid[xPos][yPos]);
+      break;
+      case "W":
+      xPos--
+      checkForScent(grid[xPos][yPos]);
+      break;
+    }
+
+  } else if (move ==="R") {
+    nextMoveRight(direction);
+  } else {
+    nextMoveLeft();
+  }
+
+  return (xPos, yPos, serialNumber, direction);
+
+}
+
+function checkForScent() {
+  console.log()
+}
+
+function nextMoveForward() {
+
+}
+
+function nextMoveRight(direction) {
+  switch(direction) {
+    case "N":
+    direction = "E";
+    break;
+    case "S":
+    direction = "W";
+    break;
+    case "E":
+    direction = "S";
+    break;
+    case "W":
+    direction = "N";
+    break;
+  }
+  return direction;
+
+}
+
+function nextMoveLeft() {
+
+}
 
 
 getGridSize(initialInput);

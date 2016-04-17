92a1f54f089ba50f6573e9d3efc98054ab59ee2a
martyjg <martinjosephgormley@gmail.com>
2016-04-17 18:01:13 +0100

    Added initial files. Linked javascript to markup, app will run in browser

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var initialInput = "5 3 \n1 1 E RFRFRFRF \n3 2 N FRRFLLFFRRFLL \n0 3 W LLFFFLFLFL";
+
+// var initialInput = prompt("Please enter the commands for your Martian Robots");
+
+var grid = [];
+
+console.log(initialInput);

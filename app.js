b5bc142e2171344767efbffa06a0c0cca16b8c1e
martyjg <martinjosephgormley@gmail.com>
2016-04-17 18:12:55 +0100

    Parsed the first two digits from the input as numbers then used as coordinates to generate the grid

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ var initialInput = "5 3 \n1 1 E RFRFRFRF \n3 2 N FRRFLLFFRRFLL \n0 3 W LLFFFLFLF
 
 var grid = [];
 
+function getGridSize(input) {
+
+  var gridWidth = parseInt(input.split(" ", 1)[0]);
+  var gridHeight = parseInt(input.split(" ", 2)[1]);
+
+  buildGrid(gridWidth, gridHeight);
+}
+
+function buildGrid(width, height) {
+
+  for (var i = 0; i <= width; i++) {
+    grid.push(new Array);
+    for (var j = 0; j <= height; j++) {
+      grid[i].push(" ");
+    }
+  }
+  
+  console.log(grid);
+}
+
+getGridSize(initialInput);
+
 console.log(initialInput);
